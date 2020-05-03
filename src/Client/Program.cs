3c7d5552972cb9c32c02fda8c719c49d7d7b7957
Jamie Lowe <jamlowathletico@gmail.com>
2020-05-03 20:18:49 +0100

    initial

---
+++ src/Client/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Net.Http;
+using IdentityModel.Client;
+using Newtonsoft.Json.Linq;
+
+namespace Client
+{
+    class Program
+    {
+        static async System.Threading.Tasks.Task Main(string[] args)
+        {
+            var errorState = true;
+            var disco = new DiscoveryDocumentResponse();
+            // discover endpoints from metadata
+            var client = new HttpClient();
+            while (errorState)
+            {
+                disco = await client.GetDiscoveryDocumentAsync("http://localhost:5000");
+                if (disco.IsError)
+                {
+                    Console.WriteLine(disco.Error);
+                    Console.ReadKey();
+                }
+                else
+                {
+                    errorState = false;
+                    Console.WriteLine("Done");
+                }
+            }
+            var tokenResponse = await client.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest
+            {
+                Address = disco.TokenEndpoint,
+
+                ClientId = "client",
+                ClientSecret = "secret",
+                Scope = "api1"
+            });
+
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);
+                return;
+            }
+            Console.WriteLine("Token Response Done");
+            // call api
+            var apiClient = new HttpClient();
+            apiClient.SetBearerToken(tokenResponse.AccessToken);
+
+            try
+            {            
+                var response = await apiClient.GetAsync("https://localhost:5001/identity");
+                if (!response.IsSuccessStatusCode)
+                {
+                    Console.WriteLine(response.StatusCode);
+                }
+                else
+                {
+                    var content = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine(JArray.Parse(content));
+                }
+
+                Console.WriteLine(tokenResponse.Json);
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+    }
+}
